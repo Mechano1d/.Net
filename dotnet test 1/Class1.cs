0534c5b7e6496e2b93d4e56e6b7cea812f06405a
Mechano1d <106731529+Mechano1d@users.noreply.github.com>
2023-09-19 10:44:34 +0300

    Добавьте файлы проекта.

---
+++ dotnet test 1/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yield
+{
+    public class _SortedList<T> : ICollection<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class _Enumenator : IEnumerator<T>
+        {
+
+            private IList<T> _list;
+            private T _current;
+            public T Current => _current;
+            object IEnumerator.Current => _current;
+
+            private int _index;
+
+            public _Enumenator(IList<T> list)
+            {
+                _index = 0;
+                _list = list;
+
+                if (_list.Any())
+                {
+                    _current = list[_index];
+                }
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool MoveNext()
+            {
+                if (_index >= _list.Count - 1)
+                {
+                    return false;
+                }
+                _index++;
+                _current = _list[_index];
+                return _index < _list.Count - 1;
+            }
+
+            public void Reset()
+            {
+                _index = 0;
+                _current = _list[0];
+            }
+        }
+    }
+}
---
+++ dotnet test 1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+
+namespace Enumerable
+{
+    internal class Program
+    {
+        private sealed class RandomEnumerator : IEnumerator
+        {
+            private int _index;
+            private readonly int _size;
+            private static readonly int _seed;
+            private int _currentValue;
+            private Random _random;
+
+            static RandomEnumerator()
+            {
+                Random seedGenerator = new Random();
+                _seed = seedGenerator.Next();
+            }
+
+            public RandomEnumerator(int size)
+            {
+                _size = size;
+                _random = new Random(_seed);
+            }
+
+            public bool MoveNext()
+            {
+                _currentValue = _random.Next();
+                _index++;
+
+                bool retVal = _index <= _size;
+
+                if (!retVal)
+                {
+                    Reset();
+                }
+
+                return retVal;
+            }
+
+            public void Reset()
+            {
+                _random = new Random(_seed);
+                _index = 0;
+            }
+
+            public object Current
+            {
+                get { return _currentValue; }
+            }
+        }
+
+        public class RandomCollection : ICollection
+        {
+            private readonly IEnumerator _random;
+
+            private readonly int _size;
+
+            public RandomCollection() : this(5)
+            {
+            }
+
+            public RandomCollection(int size)
+            {
+                _size = size;
+                _random = new RandomEnumerator(size);
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return _random;
+            }
+
+            public void CopyTo(Array array, int index)
+            {
+                GetEnumerator().Reset();
+                while (GetEnumerator().MoveNext())
+                {
+                    array.SetValue(GetEnumerator().Current, index++);
+                }
+            }
+
+            public int Count
+            {
+                get { return _size; }
+            }
+
+            public object SyncRoot
+            {
+                get { return null; }
+            }
+
+            public bool IsSynchronized
+            {
+                get { return false; }
+            }
+        }
+
+        private static void Main()
+        {
+            RandomCollection col = new RandomCollection(7);
+
+            foreach (int item in col)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            int[] copy = new int[col.Count];
+            col.CopyTo(copy, 0);
+
+            for (int x = 0; x < copy.Length; x++)
+            {
+                Console.WriteLine(copy[x]);
+            }
+        }
+    }
+}
